a257e7623d434411ccbb6a7ede283b8ae096b266
anhduc <anhduc912000@gmail.com>
2018-11-03 16:43:03 +0700

    duc_ add slick.js finish slider

--- js/javascript.js
+++ js/javascript.js
@@ -2,12 +2,6 @@ document.addEventListener("DOMContentLoaded", function(){
 
 // recent work buttom
 	var recentButton = document.querySelectorAll('.menu .nav-item');
-	var	Left 	= document.querySelectorAll('.team .left'),
-		Right 	= document.querySelectorAll('.team .right');
-	var slides = document.querySelectorAll('.card');
-	var slideIndex = 0;
-	var countSlide = slides.length;
-	console.log(countSlide);
 
 	for (var i = 0; i < recentButton.length; i++) {
 		recentButton[i].addEventListener('click', function(){
@@ -19,30 +13,45 @@ document.addEventListener("DOMContentLoaded", function(){
 		});
 	}
 
-	for (var i = 0; i < Left.length; i++) {
-		Left[i].addEventListener('click', function(){
+ 
+    $('.carousel').slick({
+        autoplay: false,
+        autoplaySpeed: 4000,
+        speed: 300,
+        prevArrow:'<i class="fas fa-angle-left"></i>',
+        nextArrow:'<i class="fas fa-angle-right"></i>',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+        ]
+    });
+    
 
-			// console.log('hi');
-			var phanTuHienTai = slides[slideIndex];
-
-			if(slideIndex < countSlide -1){
-				slideIndex++;
-			 }
-			 else{
-			 	slideIndex = 0;
-			 }
-			var phanTuTiep = slides[slideIndex];
-			console.log(phanTuTiep);
-
-		});
-	}
-
-	for (var i = 0; i < Right.length; i++) {
-		Right[i].addEventListener('click', function(){
-
-			console.log('hi');
-		});
-	}
-
-
+		
 });
